031be568602604b5a7d30ca06dcab5943cdb32cb
Tanmoy kanrar <tanmoykanrar1@gmail.com>
2023-02-14 10:53:58 +0530

    Add gitignore file

--- index.js
+++ index.js
@@ -7,73 +7,75 @@ let isGameOver = false;
 
 // Function to change the turn
 
-const changeTurn = ()=>{
-    return turn === "X" ? "0" : "X" ;
-}
+const changeTurn = () => {
+  return turn === "X" ? "0" : "X";
+};
 
 // Function to check for a win
 
-const checkWin = ()=>{
-    let boxtexts = document.getElementsByClassName("boxText");
-     let wins = [
-        [0, 1, 2, 5, 5, 0], 
-        [3, 4, 5, 5, 15, 0], 
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90], 
-        [1, 4, 7, 5, 15, 90], 
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45], 
-        [2, 4, 6, 5, 15, 135],
-     ]
-     
-     wins.forEach(e => {
-        if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== "") )
-        {
-            document.querySelector(".info").innerText = boxtexts[e[0]].innerText + " Won🏆" ;
-            isGameOver = true;
-            Winmusic.play();
-            document.querySelector(".imgBox").getElementsByTagName("img")[0].style.width = "200px" ;
-            document.querySelector(".line").style.transform = "20vw" ;
-            document.querySelector(".line").style.transform = "translate(${e[3]}, ${e[4]}) rotate(${e[5]}deg)" ;
-            
-        }
+const checkWin = () => {
+  let boxtexts = document.getElementsByClassName("boxText");
+  let wins = [
+    [0, 1, 2, 5, 5, 0],
+    [3, 4, 5, 5, 15, 0],
+    [6, 7, 8, 5, 25, 0],
+    [0, 3, 6, -5, 15, 90],
+    [1, 4, 7, 5, 15, 90],
+    [2, 5, 8, 15, 15, 90],
+    [0, 4, 8, 5, 15, 45],
+    [2, 4, 6, 5, 15, 135],
+  ];
 
-})
-    
-    
-    
+  wins.forEach((e) => {
+    if (
+      boxtexts[e[0]].innerText === boxtexts[e[1]].innerText &&
+      boxtexts[e[2]].innerText === boxtexts[e[1]].innerText &&
+      boxtexts[e[0]].innerText !== ""
+    ) {
+      document.querySelector(".info").innerText =
+        boxtexts[e[0]].innerText + " Won🏆";
+      isGameOver = true;
+      Winmusic.play();
+      document
+        .querySelector(".imgBox")
+        .getElementsByTagName("img")[0].style.width = "200px";
+      document.querySelector(".line").style.transform = "20vw";
+      document.querySelector(".line").style.transform =
+        "translate(${e[3]}, ${e[4]}) rotate(${e[5]}deg)";
     }
+  });
+};
 
 //Game logic
 
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(Element =>{
-    let boxText = Element.querySelector(".boxText")
-    Element.addEventListener("click", ()=>{
-        if(boxText.innerText === ""){
-            boxText.innerText = turn;
-            turn = changeTurn();
-            audioTurn.play();
-            checkWin();
-            if(!isGameOver ){
-                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
-            }
-
-        }
-    })
-})
+Array.from(boxes).forEach((Element) => {
+  let boxText = Element.querySelector(".boxText");
+  Element.addEventListener("click", () => {
+    if (boxText.innerText === "") {
+      boxText.innerText = turn;
+      turn = changeTurn();
+      audioTurn.play();
+      checkWin();
+      if (!isGameOver) {
+        document.getElementsByClassName("info")[0].innerText =
+          "Turn for " + turn;
+      }
+    }
+  });
+});
 
 // Add on click listner to reset button
 
-reset.addEventListener("click", ()=>{
-    let boxtexts = document.querySelectorAll(".boxText");
-    Array.from(boxtexts).forEach(Element => {
-        Element.innerText = ""
-    });
-    turn = "X";
-    isGameOver = false;
-    document.querySelector(".line").style.transform = "0vw" ;
-    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
-    document.querySelector(".imgBox").getElementsByTagName("img")[0].style.width = "0px" ;
-   
-})
+reset.addEventListener("click", () => {
+  let boxtexts = document.querySelectorAll(".boxText");
+  Array.from(boxtexts).forEach((Element) => {
+    Element.innerText = "";
+  });
+  turn = "X";
+  isGameOver = false;
+  document.querySelector(".line").style.transform = "0vw";
+  document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+  document.querySelector(".imgBox").getElementsByTagName("img")[0].style.width =
+    "0px";
+});
